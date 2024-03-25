dbe8d3cf47eae9c8d587d331e4db8f4e7a7d7f0c
nestortion <gerona.nestor@gmail.com>
2024-03-25 15:57:07 +0800

    [Accounting System]-[Added inventory page content and design]

--- client/src/routes/inventory_/index.tsx
+++ client/src/routes/inventory_/index.tsx
@@ -1,11 +1,35 @@
+import DataTable from '@/components/DataTable'
+import {
+  Inventories,
+  inventoryColumns,
+} from '@/components/table-columns/inventory.columns'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/inventory/')({
   component: Inventory,
 })
 
+const data: Inventories = {
+  invAssetName: 'Asset Name',
+  invId: 'Inv Id',
+  invStatus: 'GOOD',
+  invStocks: 200,
+}
+
 function Inventory() {
-  return <div>Inventory</div>
+  const manyData = (() => {
+    let many: Array<typeof data> = []
+
+    for (let i = 0; i < 50; i++) {
+      many.push(data)
+    }
+    return many
+  })()
+  return (
+    <div>
+      <DataTable columns={inventoryColumns} data={manyData} />
+    </div>
+  )
 }
 
 export default Inventory
