9bd602b618822475801e9888ea0339efff8a2357
nestortion <gerona.nestor@gmail.com>
2024-03-25 11:00:57 +0800

    [Accounting System]-[Implement grid for dashboard responsive design]

--- client/src/components/BarGraph.tsx
+++ client/src/components/BarGraph.tsx
@@ -1,4 +1,4 @@
-import randomColor from "randomcolor";
+import randomColor from 'randomcolor'
 import {
   Bar,
   CartesianGrid,
@@ -9,20 +9,20 @@ import {
   Tooltip,
   XAxis,
   YAxis,
-} from "recharts";
+} from 'recharts'
 
 function BarGraph({
   data,
 }: {
   data: Array<{
-    name: string;
-    Sales: number;
-    "R&D": number;
-    total: number;
-  }>;
+    name: string
+    Sales: number
+    'R&D': number
+    total: number
+  }>
 }) {
   return (
-    <ResponsiveContainer width="100%" height="100%">
+    <ResponsiveContainer className="py-4" width="100%" height="100%">
       <ComposedChart data={data} width={350} height={414}>
         <CartesianGrid stroke="#f5f5f5" />
         <XAxis dataKey="name" scale="band" />
@@ -32,19 +32,19 @@ function BarGraph({
         <Bar
           dataKey="Sales"
           barSize={20}
-          stackId={"a"}
-          fill={randomColor({ luminosity: "light", hue: "purple" })}
+          stackId={'a'}
+          fill={randomColor({ luminosity: 'light', hue: 'red' })}
         />
         <Bar
           dataKey="R&D"
           barSize={20}
-          stackId={"a"}
-          fill={randomColor({ luminosity: "light", hue: "purple" })}
+          stackId={'a'}
+          fill={randomColor({ luminosity: 'light', hue: 'red' })}
         />
         <Line type="monotone" dataKey="total" stroke="#ff7300" />
       </ComposedChart>
     </ResponsiveContainer>
-  );
+  )
 }
 
-export default BarGraph;
+export default BarGraph
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,20 +1,25 @@
-import Expenses from "@/components/Expenses";
-import Revenues from "@/components/Revenues";
-import { Text } from "@/components/ui/text";
-import { createFileRoute } from "@tanstack/react-router";
+import Expenses from '@/components/Expenses'
+import Revenues from '@/components/Revenues'
+import { Text } from '@/components/ui/text'
+import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute("/")({
+export const Route = createFileRoute('/')({
   component: Home,
-});
+})
 
 function Home() {
   return (
-    <div className="min-h-[85vh] px-4 overflow-y-auto w-screens">
+    <div className="max-h-[85vh] px-4 overflow-y-auto w-screens">
       <div className="border-b-2 border-black pb-4 mb-4">
-        <Text variant={"heading1bold"}>Dashboard</Text>
+        <Text variant={'heading1bold'}>Dashboard</Text>
       </div>
-      <Expenses />
-      <Revenues />
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(400px,1fr))] gap-4">
+        <Expenses />
+        <Expenses />
+        <Expenses />
+        <Expenses />
+        <Revenues />
+      </div>
     </div>
-  );
+  )
 }
