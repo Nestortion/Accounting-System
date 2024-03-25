7ad17f2b10f37908b922b8060aa4abf54e02796a
nestortion <gerona.nestor@gmail.com>
2024-03-25 15:42:01 +0800

    [Accounting System]-[Added payroll page content and design]

--- client/src/routes/payrolls_/index.tsx
+++ client/src/routes/payrolls_/index.tsx
@@ -1,11 +1,36 @@
+import DataTable from '@/components/DataTable'
+import {
+  payrollColumns,
+  type Payrolls,
+} from '@/components/table-columns/payrolls.columns'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/payrolls/')({
   component: Payrolls,
 })
 
+const data: Payrolls = {
+  prDateFrom: new Date().toLocaleDateString(),
+  prDateTo: new Date().toLocaleDateString(),
+  prEmpName: 'Nestor Gerona',
+  prFinalAmount: 1000,
+  prTotalDeduction: 200,
+}
+
 function Payrolls() {
-  return <div>Payrolls</div>
+  const manyData = (() => {
+    let many: Array<typeof data> = []
+
+    for (let i = 0; i < 50; i++) {
+      many.push(data)
+    }
+    return many
+  })()
+  return (
+    <div className="min-h-[85vh]">
+      <DataTable columns={payrollColumns} data={manyData}></DataTable>
+    </div>
+  )
 }
 
 export default Payrolls
