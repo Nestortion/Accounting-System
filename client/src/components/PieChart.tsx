4dc0d9ecc6b466d56c09328c761e47e8622ab806
nestortion <gerona.nestor@gmail.com>
2024-03-26 09:12:52 +0800

    [Accounting System]-[Add tooltip to piechart]

--- client/src/components/PieChart.tsx
+++ client/src/components/PieChart.tsx
@@ -1,13 +1,40 @@
-import { Cell, Pie, PieChart as RechartPie } from "recharts";
+import {
+  Cell,
+  Pie,
+  PieChart as RechartPie,
+  Tooltip,
+  TooltipProps,
+} from 'recharts'
+import {
+  NameType,
+  ValueType,
+} from 'recharts/types/component/DefaultTooltipContent'
+import { text } from './ui/text'
+
+const CustomToolTip = ({
+  active,
+  payload,
+}: TooltipProps<ValueType, NameType>) =>
+  active &&
+  payload && (
+    <div
+      className={text({
+        variant: 'body',
+        class: `rounded-md px-3 bg-popover border`,
+      })}
+    >
+      {payload[0].name}
+    </div>
+  )
 
 function PieChart({
   data,
 }: {
   data: Array<{
-    name: string;
-    color: string;
-    value: number;
-  }>;
+    name: string
+    color: string
+    value: number
+  }>
 }) {
   return (
     <RechartPie width={200} height={180}>
@@ -21,11 +48,12 @@ function PieChart({
         fill="#8884d8"
       >
         {data.map((d, index) => (
-          <Cell key={index} fill={d.color} />
+          <Cell key={index} fill={d.color} className="outline-none" />
         ))}
       </Pie>
+      <Tooltip content={CustomToolTip} />
     </RechartPie>
-  );
+  )
 }
 
-export default PieChart;
+export default PieChart
